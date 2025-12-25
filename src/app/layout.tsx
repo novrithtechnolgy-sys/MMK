3193913c81a353f73771316ddd62097738cebac4
GSNSAMPATH <novrithtechnolgy@gmail.com>
2025-12-25 17:44:45 +0530

    update images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -111,9 +111,8 @@ export default function RootLayout({
             });
           `}
         </Script>
-      <link rel="icon" type="image/png" sizes="37x37" href="/favicon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body
         className={`${poppins.variable} ${unbounded.variable} antialiased`}
